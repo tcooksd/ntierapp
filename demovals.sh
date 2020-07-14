f8e6a83db2c5bf12e6d8d19bb74a469567a9bb2c
tcooksd@gmail.com <tcooksd@gmail.com>
2020-07-14 17:41:49 -0400

    asdf

--- demovals.sh
+++ demovals.sh
@@ -2,4 +2,4 @@ echo "<%= customOptions.tcontier01 %>" >> /tmp/info
 echo "<%= customOptions.tcontier02 %>" >> /tmp/info
 echo "<%= container.dataPath %>" >> /tmp/info
 echo "$OUTPUT_BASE_DIR" >> /tmp/info
-demo.sh 
+demo.sh

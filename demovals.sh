544db35a11eda8b2b1cbdc749614a6cc67f6b37f
tcooksd@gmail.com <tcooksd@gmail.com>
2020-07-14 18:28:29 -0400

    asdf

--- demovals.sh
+++ demovals.sh
@@ -2,4 +2,10 @@
 #!/bin/bash
 echo "<%= customOptions.tcontier01 %>" >> /tmp/info
 echo "<%= customOptions.tcontier02 %>" >> /tmp/info
-./demo.sh 
+echo "outputting directory of launch"
+echo "########################################"
+ls
+echo "########################################"
+
+echo "running demo.sh command"
+./demo.sh

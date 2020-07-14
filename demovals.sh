46f9db5bc94c5c3babddf946216f2c6bfcbd61d9
tcooksd@gmail.com <tcooksd@gmail.com>
2020-07-14 17:36:36 -0400

    testing datapath return

--- demovals.sh
+++ demovals.sh
@@ -1,3 +1,4 @@
 echo "<%= customOptions.tcontier01 %>" >> /tmp/info
 echo "<%= customOptions.tcontier02 %>" >> /tmp/info
+echo "<%= container.dataPath %>" >> /tmp/info
 echo "$OUTPUT_BASE_DIR" >> /tmp/info

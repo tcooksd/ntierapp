033aa75db408dfc16dbd695c98cfebdc51dba14f
tcooksd@gmail.com <tcooksd@gmail.com>
2020-07-14 14:58:36 -0400

    test

--- demo.sh
+++ demo.sh
@@ -12,6 +12,6 @@ git clone https://github.com/opencart/opencart.git
 cd /tmp/opencart/upload/install/
 
 php  cli_install.php install --username admin --email "<%= customOptions.tcontier01 %>" \
- --password "<%= customOptions.tcontier01 %>" --http_server http://localhost/opencart/ \
+ --password "<%= customOptions.tcontier02 %>" --http_server http://localhost/opencart/ \
  --cloud 0 --db_driver mysqli --db_hostname localhost --db_username root \
   --db_password pass --db_database opencart --db_port 3306 --db_prefix oc_
